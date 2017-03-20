56105b2dc28a7b2c77dfd4886487fbe985edf7c3
MarcosRS <MarcosRS16@gmail.com>
2017-03-20 17:05:58 -0400

    mic2

--- microphone.js
+++ microphone.js
@@ -1,8 +1,27 @@
-const av = require("tessel-av");
-const mic = new av.Microphone();
+"use strict";
 
-// Initialize the microphone input
-mic.listen();
-// This will automattically pipe the current
-// input to an available output.
-mic.monitor();
+const av = require("tessel-av");
+const cp = require("child_process");
+const Transform = require("stream").Transform;
+
+class NopTransform extends Transform {
+  constructor(options) {
+    super(options);
+  }
+  _transform(chunk, encoding, callback) {
+    this.push(chunk);
+    callback();
+  }
+}
+
+const nop = new NopTransform();
+const mic = new av.Microphone();
+// Make an aplay output
+const aplay = cp.spawn('aplay', ['-r', 9600]);
+
+
+// 1. Create an input with a rate of 9600
+// 2. Pipe it through an instance of NopTransform,
+//    wich actually does nothing.
+// 3. Output to the aplay process
+mic.listen(['-r', 9600]).pipe(nop).pipe(aplay.stdin);
